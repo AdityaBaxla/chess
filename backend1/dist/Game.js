e08747b83028e9e36b23c3ba048420c2217cbd60
Aditya-IITM-BS <158017180+Aditya-IITM-BS@users.noreply.github.com>
2024-04-19 01:36:04 +0530

    55:01 backend moves sending and validating

---
+++ backend1/dist/Game.js
@@ -0,0 +1,65 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Game = void 0;
+const chess_js_1 = require("chess.js");
+const messages_1 = require("./messages");
+class Game {
+    constructor(player1, player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = new chess_js_1.Chess();
+        this.moves = [];
+        this.startTime = new Date();
+        console.log("consturciotn for game initialized");
+        this.player1.send(JSON.stringify({
+            type: messages_1.INIT_GAME,
+            payload: {
+                color: "white",
+            },
+        }));
+        this.player2.send(JSON.stringify({
+            type: messages_1.INIT_GAME,
+            payload: {
+                color: "black",
+            },
+        }));
+    }
+    makeMove(socket, move) {
+        // validation
+        try {
+            this.board.move(move);
+        }
+        catch (e) {
+            // setup logic for illegal move, not the current player's move
+            console.log(e);
+            return;
+        }
+        if (this.board.isGameOver()) {
+            this.player1.send(JSON.stringify({
+                type: messages_1.GAME_OVER,
+                payload: {
+                    winner: this.board.turn() === "w" ? "black" : "white",
+                },
+            }));
+            return;
+        }
+        //  is this this user's move
+        // is the move valid
+        //update the board
+        // push the move
+        //send the updated move
+        if (this.board.moves.length % 2 == 0) {
+            this.player2.send(JSON.stringify({
+                type: messages_1.MOVE,
+                payload: move,
+            }));
+        }
+        else {
+            this.player1.send(JSON.stringify({
+                type: messages_1.MOVE,
+                payload: move,
+            }));
+        }
+    }
+}
+exports.Game = Game;
---
+++ backend1/src/Game.ts
@@ -0,0 +1,84 @@
+import { WebSocket } from "ws";
+import { Chess } from "chess.js";
+
+import { GAME_OVER, INIT_GAME, MOVE } from "./messages";
+
+export class Game {
+  public player1: WebSocket;
+  public player2: WebSocket;
+  public board: Chess;
+  private moves: string[];
+  private startTime: Date;
+
+  constructor(player1: WebSocket, player2: WebSocket) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.board = new Chess();
+    this.moves = [];
+    this.startTime = new Date();
+    console.log("consturciotn for game initialized");
+    this.player1.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "white",
+        },
+      })
+    );
+
+    this.player2.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "black",
+        },
+      })
+    );
+  }
+
+  makeMove(socket: WebSocket, move: string) {
+    // validation
+    console.log("went inside make move");
+    try {
+      this.board.move(move);
+      console.log("made move");
+      this.moves.push(move);
+    } catch (e) {
+      // setup logic for illegal move, not the current player's move
+      console.log(e);
+      return;
+    }
+
+    if (this.board.isGameOver()) {
+      this.player1.send(
+        JSON.stringify({
+          type: GAME_OVER,
+          payload: {
+            winner: this.board.turn() === "w" ? "black" : "white",
+          },
+        })
+      );
+      return;
+    }
+    //  is this this user's move
+    // is the move valid
+    //update the board
+    // push the move
+    //send the updated move
+    if (this.board.moves.length % 2 == 0) {
+      this.player2.send(
+        JSON.stringify({
+          type: MOVE,
+          payload: move,
+        })
+      );
+    } else {
+      this.player1.send(
+        JSON.stringify({
+          type: MOVE,
+          payload: move,
+        })
+      );
+    }
+  }
+}
